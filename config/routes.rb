48e815f10d4e4700b78a758304c484908a1be5e9
Josh T <jtaimang@uccs.edu>
2017-12-11 09:07:56 +0000

    Auxillary databases successfully seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :starches
   resources :vegetables
   resources :meats
+  resources :starches
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211083003) do
+ActiveRecord::Schema.define(version: 20171211090328) do
 
   create_table "meats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
